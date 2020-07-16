05634136cf03d45e781ef6636fbcb10f15899623
geonwoo.jeong <doscmDev@gmail.com>
2020-07-16 13:48:29 +0900

    feat: apply eslint prettier environment

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2020: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+  },
+};
--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Image } from "react-native";
-import { AppLoading } from "expo";
-import styled from "styled-components/native";
-import { Asset } from "expo-asset";
+import React, { useState } from 'react';
+import { Image } from 'react-native';
+import { AppLoading } from 'expo';
+import styled from 'styled-components/native';
+import { Asset } from 'expo-asset';
 
 const Word = styled.Text``;
 
-const cashImages = (images: Array<any>) => {
+const cashImages = (images: Array<string | React.ReactText>) => {
   images.map((image) => {
-    if (typeof image === "string") {
+    if (typeof image === 'string') {
       return Image.prefetch(image);
     } else {
       return Asset.fromModule(image).downloadAsync;
@@ -16,12 +16,12 @@ const cashImages = (images: Array<any>) => {
   });
 };
 
-export default function App() {
+const App = (): React.ReactElement => {
   const [isReady, setIsReady] = useState(false);
   const loadAssets = async () => {
     const images = cashImages([
-      "https://images.unsplash.com/photo-1594569567351-ad9e90210643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-      require("./assets/splash.png"),
+      'https://images.unsplash.com/photo-1594569567351-ad9e90210643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+      require('./assets/splash.png'),
     ]);
   };
   const onFinish = () => {
@@ -35,8 +35,10 @@ export default function App() {
       startAsync={loadAssets}
       onFinish={onFinish}
       onError={(e) => {
-        console.log("LOAD ERROR", e);
+        console.log('LOAD ERROR', e);
       }}
     />
   );
-}
+};
+
+export default App;
