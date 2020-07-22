34120d864176a891d9662d24b51451e72386e47b
geonwoo.jeong <doscmDev@gmail.com>
2020-07-23 02:46:33 +0900

    feat: add mypage animation example

--- src/Screens/Detail.tsx
+++ src/Screens/Detail.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
+import { Animated } from 'react-native';
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
 const Text = styled.Text``;
 
 const Detail = () => {
+  const [position, setPosition] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
+  const moveX = () => {
+    Animated.timing(position, {
+      toValue: { x: 100, y: 0 },
+      useNativeDriver: true,
+      delay: 200,
+    }).start();
+  };
+
+  useEffect(() => {
+    moveX();
+  }, []);
   return (
     <Container>
-      <Text>DETAIL</Text>
+      <Animated.View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'green',
+          transform: [{ translateX: position.x }],
+        }}></Animated.View>
     </Container>
   );
 };
