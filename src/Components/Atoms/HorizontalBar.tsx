b48bc832037d94950d93523b28a42003f2f0bfd1
geonwoo.jeong <doscmDev@gmail.com>
2020-07-30 16:51:28 +0900

    feat: add HorizontalBar component

---
+++ src/Components/Atoms/HorizontalBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components/native';
+import { MarginStyleProps } from '~/@types';
+import { marginStyles } from '~/styles/mixin';
+import { GREY } from '~/constants/Colors';
+
+const Hr = styled.View<StyleProps>`
+  width: 100%;
+  ${marginStyles};
+  background-color: ${({ color }) => color ?? `${GREY}`};
+  height: ${({ height }) => height ?? '1px'};
+`;
+
+interface StyleProps extends MarginStyleProps {
+  color?: string;
+  height?: string;
+}
+
+const HorizontalBar = ({
+  marginLeft,
+  marginTop,
+  marginBottom,
+  marginRight,
+  color,
+  height,
+}: StyleProps) => {
+  return (
+    <Hr
+      marginLeft={marginLeft}
+      marginTop={marginTop}
+      marginBottom={marginBottom}
+      marginRight={marginRight}
+      color={color}
+      height={height}
+    />
+  );
+};
+
+export default HorizontalBar;
