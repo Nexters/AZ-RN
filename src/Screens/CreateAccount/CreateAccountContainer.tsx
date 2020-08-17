f83605545384e6a969ff82c0f92e0f752f955dec
geonwoo.jeong <doscmDev@gmail.com>
2020-08-17 22:32:39 +0900

    refactor: update input validation

--- src/Screens/CreateAccount/CreateAccountContainer.tsx
+++ src/Screens/CreateAccount/CreateAccountContainer.tsx
@@ -3,19 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { useHandleInput } from '~/hooks';
 import CreateAccountViewer from './CreateAccountViewer';
-import { useToggle } from '~/hooks';
-import { arrayConditionCheck, textValidation } from '~/lib';
-import { postCreataeAccountThunk } from '~/store/modules/auth/thunks';
-import {
-  postCreateAccount,
-  postIdentificationCheck,
-  postNicknameCheck,
-} from '~/api';
+import { postIdentificationCheck, postNicknameCheck, postCreateAccount } from '~/api';
 import { RootState } from '~/store/modules';
-import {
-  postVerifyIdThunk,
-  postVerifyNicknameThunk,
-} from '~/store/modules/user/thunks';
+import { postVerifyIdThunk, postVerifyNicknameThunk } from '~/store/modules/user/thunks';
+import { textValidation, arrayConditionCheck } from '~/lib';
+import { postCreataeAccountThunk } from '~/store/modules/auth/thunks';
 
 const CreateAccountContainer = () => {
   const dispatch = useDispatch();
@@ -27,85 +19,157 @@ const CreateAccountContainer = () => {
     },
   } = useSelector((state: RootState) => state);
 
-  const { onlyLowercaseAndNumberValidator, passwordValidator } = textValidation;
-
-  useEffect(() => {
-    //
-  }, [auth]);
+  const {
+    onlyLowercaseAndNumberValidator,
+    passwordValidator,
+    exceptSpecialCaractor,
+  } = textValidation;
 
   const { bind: idBinder } = useHandleInput('');
   const { bind: pwdBinder } = useHandleInput('');
   const { bind: verifyBinder } = useHandleInput('');
   const { bind: nicknameBinder } = useHandleInput('');
 
-  const userIdDuplicateCheck = () => {
-    const config = {
-      ...postIdentificationCheck,
-      identification: idBinder.text,
-    };
-    if (
-      idBinder.text.length > 1 &&
-      onlyLowercaseAndNumberValidator(idBinder.text)
-    ) {
+  type ErrorType = {
+    available?: boolean;
+    errorMsg: string;
+  };
+  const [idErrorMsg, setIdErrorMsg] = useState<ErrorType>({
+    available: undefined,
+    errorMsg: '',
+  });
+  const [pwdErrorMsg, setPwdErrorMsg] = useState<ErrorType>({
+    available: undefined,
+    errorMsg: '',
+  });
+  const [nicknameErrorMsg, setNicknameErrorMsg] = useState<ErrorType>({
+    available: undefined,
+    errorMsg: '',
+  });
+  const [isActivation, setIsActivation] = useState(false);
+
+  const handleIdVerify = () => {
+    const { text } = idBinder;
+    if (text.length === 0) return;
+    if (text.length > 2 && onlyLowercaseAndNumberValidator(text)) {
+      const config = {
+        ...postIdentificationCheck,
+        identification: text,
+      };
       dispatch(postVerifyIdThunk(config));
+      setIdErrorMsg({
+        available: false,
+        errorMsg: '',
+      });
+    } else {
+      setIdErrorMsg({
+        available: true,
+        errorMsg: '2개 이상 영어 및 숫자로 만들라구~',
+      });
     }
   };
-  const passwordEqualCheck = () => {
-    if (pwdBinder.text !== verifyBinder.text) {
-      return false;
+
+  const handlePwdVerify = () => {
+    const { text: password1 } = pwdBinder;
+    const { text: password2 } = verifyBinder;
+    if (password2.length === 0 && password1.length === 0) return;
+    if (password1 !== password2) {
+      setPwdErrorMsg({
+        available: true,
+        errorMsg: '비밀번호가 다릅니다',
+      });
     } else {
-      if (passwordValidator(verifyBinder.text)) {
-        return true;
+      if (passwordValidator(password2)) {
+        setPwdErrorMsg({
+          available: false,
+          errorMsg: '',
+        });
       } else {
-        //  add password validation wording
-        return false;
+        setPwdErrorMsg({
+          available: true,
+          errorMsg: '8자 이상 영문 숫자 조합으로 부탁혀~',
+        });
       }
     }
   };
-  const nicknameDuplicateCheck = () => {
-    const config = {
-      ...postNicknameCheck,
-      identification: nicknameBinder.text,
-    };
-    dispatch(postVerifyNicknameThunk(config));
+
+  const handleNicknameVerify = () => {
+    const { text } = nicknameBinder;
+    if (text.length > 1 && exceptSpecialCaractor(text)) {
+      const config = {
+        ...postNicknameCheck,
+        nickname: text,
+      };
+      dispatch(postVerifyNicknameThunk(config));
+      setNicknameErrorMsg({
+        available: false,
+        errorMsg: '',
+      });
+    } else {
+      setNicknameErrorMsg({
+        available: true,
+        errorMsg: '2자 이상 그리고 특수문자 제외 해!',
+      });
+    }
   };
 
-  const passwordHooks = useToggle(undefined, passwordEqualCheck);
+  useEffect(() => {
+    console.log('isNicknameUsed', isNicknameUsed);
 
-  const availables = [
+    if (isIdUsed) {
+      setIdErrorMsg({
+        available: true,
+        errorMsg: '아이디 중복이라구',
+      });
+    }
+    if (isNicknameUsed) {
+      setNicknameErrorMsg({
+        available: true,
+        errorMsg: '닉네임 중복입니다만..',
+      });
+    }
+  }, [isIdUsed, isNicknameUsed]);
+
+  useEffect(() => {
+    const conditions = [idErrorMsg.available, pwdErrorMsg.available, nicknameErrorMsg.available];
+    const reuslt = arrayConditionCheck(
+      conditions,
+      (condition) => condition !== undefined && !condition,
+    );
+    setIsActivation(reuslt);
+  }, [idErrorMsg, pwdErrorMsg, nicknameErrorMsg]);
+
+  const inputOptions = [
     {
       id: 'id',
       placeholder: '아이디',
-      guideMsg: '아이디 중복',
       bind: idBinder,
-      onToggle: userIdDuplicateCheck,
-      isAvailable: isIdUsed,
+      onBlur: handleIdVerify,
+      errorOption: idErrorMsg,
     },
     {
       id: 'password',
-      ...passwordHooks,
       placeholder: '비밀번호 입력',
-      guideMsg: '',
       bind: pwdBinder,
+      errorOption: pwdErrorMsg,
+      secureTextEntry: true,
     },
     {
       id: 'password2',
-      ...passwordHooks,
       placeholder: '비밀번호 확인',
-      guideMsg: '비밀번호 틀림',
       bind: verifyBinder,
+      onBlur: handlePwdVerify,
+      errorOption: pwdErrorMsg,
+      secureTextEntry: true,
     },
     {
       id: 'nickname',
       placeholder: '별명 입력',
-      guideMsg: '별명 중복',
       bind: nicknameBinder,
-      onToggle: nicknameDuplicateCheck,
-      isAvailable: isNicknameUsed,
+      onBlur: handleNicknameVerify,
+      errorOption: nicknameErrorMsg,
     },
   ];
-
-  const [isActivationSignup, setIsActivationSignup] = useState<boolean>(false);
 
   const handleCreateAccount = () => {
     const config = {
@@ -114,24 +178,14 @@ const CreateAccountContainer = () => {
       nickname: nicknameBinder.text,
       password: pwdBinder.text,
     };
-
     dispatch(postCreataeAccountThunk(config));
   };
 
-  useEffect(() => {
-    const isReady = arrayConditionCheck(
-      availables,
-      (available) => available.isAvailable === true,
-    );
-
-    setIsActivationSignup(isReady);
-  }, [isIdUsed, isNicknameUsed, passwordHooks.isAvailable]);
-
   return (
     <CreateAccountViewer
-      availables={availables}
-      isActivationSignup={isActivationSignup}
+      inputOptions={inputOptions}
       handleCreateAccount={handleCreateAccount}
+      isActivation={isActivation}
     />
   );
 };
--- src/lib/textValidation.tsx
+++ src/lib/textValidation.tsx
@@ -4,8 +4,17 @@ const onlyLowercaseAndNumberValidator = (text: string) => {
 };
 
 const passwordValidator = (text: string) => {
-  const regex = /(?=.*d)(?=.*[a-z]).{8,}/;
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
   return regex.test(text);
 };
 
-export default { onlyLowercaseAndNumberValidator, passwordValidator };
+const exceptSpecialCaractor = (text: string) => {
+  const regex = /^[a-z0-9ㄱ-힣]*$/g;
+  return regex.test(text);
+};
+
+export default {
+  onlyLowercaseAndNumberValidator,
+  passwordValidator,
+  exceptSpecialCaractor,
+};
