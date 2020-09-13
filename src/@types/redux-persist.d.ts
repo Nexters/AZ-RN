98ab8d8977c3310db2ca18c89cbcb15ad9606a93
geonwoo.jeong <doscmDev@gmail.com>
2020-09-13 22:46:33 +0900

    Update redux-persist.d.ts

--- src/@types/redux-persist.d.ts
+++ src/@types/redux-persist.d.ts
@@ -2,12 +2,6 @@ declare module 'redux-persist/es/persistStore' {
   import { Store, Action, AnyAction } from 'redux';
   import { PersistorOptions, Persistor } from 'redux-persist/es/types';
 
-  /**
-   * @desc Creates a persistor for a given store.
-   * @param store store to be persisted (or match an existent storage)
-   * @param persistorOptions enhancers of the persistor
-   * @param callback bootstrap callback of sort.
-   */
   // tslint:disable-next-line: strict-export-declare-modifiers
   export default function persistStore<S = any, A extends Action<any> = AnyAction>(
     store: Store<S, A>,
